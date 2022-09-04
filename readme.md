52fc8ce50eb93517a5787e8a5e0c4770d1bc97a1
dg <jason@xax.li>
2022-09-03 23:24:55 -0600

    posting to github

---
+++ readme.md
@@ -0,0 +1,9 @@
+### numHardLock
+
+idk why but I had a hard time keeping my number lock on. no idea what was turning it off.
+
+I made this in Go originally, now it's in rust because why not.
+
+I have this setup as a scheduled task which you can view in `numHardLock.xml`.
+
+if you find this useful, great.
---
+++ src/main.rs
@@ -0,0 +1,81 @@
+#![allow(non_snake_case)]
+use std::thread::sleep;
+use std::time::Duration;
+use windows::{
+    Win32::UI::Input::KeyboardAndMouse::{
+        INPUT,
+        INPUT_0,
+        KEYBDINPUT,
+        VIRTUAL_KEY,
+        SendInput,
+        GetKeyState,
+        INPUT_KEYBOARD,
+        KEYEVENTF_KEYUP,
+    },
+};
+
+// idk why i need to do this
+const NUMBER_LOCK:u16 = 144;
+
+// get state of number lock, return bool
+fn get_numlock_state() -> bool {
+    unsafe {
+        let state = GetKeyState(NUMBER_LOCK as i32);
+        if state == 1 {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+unsafe fn enable_numlock() {
+    // define a key down and key up array of inputs
+    let mut keypress = [
+        INPUT {
+            r#type: INPUT_KEYBOARD,
+            Anonymous: INPUT_0 {
+                ki: KEYBDINPUT {
+                    wVk: VIRTUAL_KEY(NUMBER_LOCK),
+                    wScan: 0,
+                    dwFlags: Default::default(),
+                    time: 0,
+                    dwExtraInfo: 0,
+                }
+            }
+        },
+        INPUT {
+            r#type: INPUT_KEYBOARD,
+            Anonymous: INPUT_0 {
+                ki: KEYBDINPUT {
+                    wVk: VIRTUAL_KEY(NUMBER_LOCK),
+                    wScan: 0,
+                    dwFlags: KEYEVENTF_KEYUP,
+                    time: 0,
+                    dwExtraInfo: 0,
+                }
+            }
+        }
+    ];
+
+    // use SendInput to turn on NUMBER_LOCK
+    if SendInput(&mut keypress, std::mem::size_of::<INPUT>() as _) == 1 {
+        println!("failed to send numLock keypress")
+    }
+
+}
+
+fn main() {
+    unsafe {
+        println!("numHardLock starting");
+
+        loop {
+            if !get_numlock_state() {
+                //println!("numLock off, turning on");
+                enable_numlock()
+            }
+            sleep(Duration::from_millis(250))
+        }
+
+    }
+}
